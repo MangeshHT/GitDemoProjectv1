54aac5126de465660a4c1457088fd0be0535c2fa
Mangesh <mangeshthakare4326@gmail.com>
2022-12-22 15:20:04 +0530

    First Commit

---
+++ src/com/git/demo/Test1.java
@@ -0,0 +1,15 @@
+package com.git.demo;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		String hungry = "no";
+		
+		if(hungry.equals("Yes")) {
+			System.out.println("Eat something");
+		}
+		else 
+			System.out.println("Don't eat");
+	}
+
+}
